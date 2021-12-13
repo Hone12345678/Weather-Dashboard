67219b5feab3b513c0857ed5ded0f4fb27122e03
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2021-12-13 14:00:23 -0700

    fixed API interaction

--- assets/js/weatherDashLogic.js
+++ assets/js/weatherDashLogic.js
@@ -1,13 +1,11 @@
 
-var lat = 33.44
-var lon = -94.04
 var getWeatherData = function () {
-    var weatherAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={f16d7083a167785de27297dbf5a2e73c}';
+    var weatherAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=f16d7083a167785de27297dbf5a2e73c';
     fetch(weatherAPI).then(function (response) {
         console.log(response);
         if (response.ok) {
             response.json().then(function (data) {
-                renderItem(data);
+                console.log(data)
             });
         } else {
             alert("input invalid");
@@ -15,11 +13,7 @@ var getWeatherData = function () {
         }
 
     })
-        .then(function (data) {
-            (data)
-            console.log(data)
-
-        });
+      
 };
 
 getWeatherData();
